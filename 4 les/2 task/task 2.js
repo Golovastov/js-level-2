125fce6931c41e69f915125c0d06bb377739167e
Konstantin <kotjane@ya.ru>
2018-03-23 14:17:17 +0300

    14:16

---
+++ 4 les/2 task/task 2.js
@@ -0,0 +1,38 @@
+(function ($) {
+  $(function () {
+
+  });
+})(jQuery);
+
+
+function validation() {
+  var doc = document,
+    valueName = doc.getElementById('name').value,
+    valueTel = doc.getElementById('tel').value,
+    valueMail = doc.getElementById('mail').value;
+  //RegExp для имени
+  if (/^[a-zа-яё]{2,}$/i.test(valueName) === true) {
+    // для телефона
+    if (/\+?\d{1,3}\(?\s?\d{3}\s?\)?\d{3}-?\s?\d{4}/.test(valueTel) === true) {
+      // для E-mail
+      if (/(^\w+(|(\.|\-)\w+))(?=@[a-z]{2,}\.[a-z]{2,4}\b)/i.test(valueMail) === true) {
+        alert('отправлено');
+      } else
+        var mail = doc.getElementById('mail');
+      mail.classList.add('err');
+      mail.oninput = function () {
+        mail.classList.remove('err');
+      }
+    } else
+      var tel = doc.getElementById('tel');
+    tel.classList.add('err');
+    tel.oninput = function () {
+      tel.classList.remove('err');
+    }
+  } else
+    var name = doc.getElementById('name');
+  name.classList.add('err');
+  name.oninput = function () {
+    name.classList.remove('err');
+  }
+}

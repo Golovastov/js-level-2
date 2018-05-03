bd1790a3274b2724280e14914ad56babd8816514
Konstantin <kotjane@ya.ru>
2018-05-03 21:12:42 +0300

    home upd

--- kursovoj_proekt/app/js/catalog.js
+++ kursovoj_proekt/app/js/catalog.js
@@ -11,11 +11,21 @@ var catalog = (function($) {
     // Рендерим каталог
     function _render() {
         var template = _.template($('#catalog-template').html()),
-            $goods = $('#featured_items_slider_1');
+            $goods = $('#featured_items');
 
         $.getJSON('https://raw.githubusercontent.com/Golovastov/js-level-2/master/kursovoj_proekt/app/response/futuredItems.json', function(data) {
             $goods.html(template({goods: data}));
-        });
+            
+            // Инициализация слайдера
+            $goods.slick({
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: true
+              });
+        });       
     }
 
 
